0f732d7c3fdf6421c334b03023405993da4c7f83
karba <karbalive@hotmail.com>
2023-08-24 16:31:14 +0200

    fix comparebool
    yep

--- BallsOfSteel/Assets/Scripts/Game/Joakim_Karbing/CompareBool_Joakim_Karbing.cs
+++ BallsOfSteel/Assets/Scripts/Game/Joakim_Karbing/CompareBool_Joakim_Karbing.cs
@@ -6,21 +6,11 @@ namespace AI.Nodes
 {
     public class CompareBool_Joakim_Karbing : DecoratorNode
     {
-        public enum Operator
-        {
-            Equals, 
-            NotEquals,
-        };
-
         public string           m_key = "VariableName";
-        public Operator         m_operator = Operator.NotEquals;
         public bool              m_iValue = false;
-
         static string[]         sm_opCodes = new string[] { " == ", " != "};
 
         #region Properties
-
-        public override string Description => m_key + sm_opCodes[(int)m_operator] + m_iValue.ToString();
 
         #endregion
 
@@ -29,21 +19,15 @@ namespace AI.Nodes
             m_state = State.Failure;
             if (Tree != null && Tree.Blackboard != null)
             {
-                int iValue = Tree.Blackboard.GetValue(m_key, 0);
-
-                switch (m_operator)
-                {
-                    case Operator.Equals:
-                        m_state = iValue == m_iValue ? State.Running : State.Failure;
-                        break;
-
-                    case Operator.NotEquals:
-                        m_state = iValue != m_iValue ? State.Running : State.Failure;
-                        break;
+                bool iValue = Tree.Blackboard.GetValue(m_key, new bool());
+                if (iValue) {
+                    m_state = State.Success;
+                }
+                else {
+                    m_state = State.Failure;
                 }
             }
 
-            // update child?
             if (m_state == State.Running)
             {
                 m_state = m_child.Update();
